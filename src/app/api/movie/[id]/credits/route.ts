19ee63a800569c223bab472ed3ee7f1d91c0baff
DavidTelles <davidsilvatelles2023@gmail.com>
2026-09-02 15:06:24 -0300

    fix: fix error in deployment

--- src/app/api/movie/[id]/credits/route.ts
+++ src/app/api/movie/[id]/credits/route.ts
@@ -1,6 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
+    request: NextRequest,
     { params }: { params: Promise<{ id: string }> }
 ) {
     const { id } = await params;
