68db22a9724371e86ee7e6eae501b3c67ea3b15e
David Silva Telles <davidsilvatelles2023@gmail.com>
2026-09-09 17:09:04 -0300

    feat: add analytics

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Topbar from "../components/Topbar";
 import { LanguageProvider } from "@/src/contexts/LanguageContext";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
     title: "Cineflow",
@@ -10,6 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
         <html lang="en" className="scrollbar-none">
+            <Analytics />
             <body className="bg-white dark:bg-zinc-800">
                 <LanguageProvider >
                     <Topbar />
